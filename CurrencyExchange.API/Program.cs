512d8059895f751da73eab7a9d8ebbb904021e90
DanyaLozhkin <delaynore@gmail.com>
2023-12-06 19:04:24 +0400

    register db services

--- CurrencyExchange.API/Program.cs
+++ CurrencyExchange.API/Program.cs
@@ -1,5 +1,14 @@
+using CurrencyExchange.API.Data;
+using CurrencyExchange.API.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<CurrencyExchangeDbContext>();
+builder.Services.AddScoped<ICurrencyRepository, CurrencyRepository>();
+builder.Services.AddScoped<IExchangeRateRepository, ExchangeRateRepository>();
+
+
 var app = builder.Build();
+
 
 app.MapGet("/", () => "Hello World!");
 
