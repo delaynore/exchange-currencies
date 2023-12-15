adcbbc6c90a3cf8374abef6bb3ac5a68e3e9175e
DanyaLozhkin <delaynore@gmail.com>
2023-12-15 17:51:46 +0400

    feat: new response type of service and errors

---
+++ CurrencyExchange.API/Response/Error.cs
@@ -0,0 +1,9 @@
+﻿namespace CurrencyExchange.API.Response
+{
+    public record Error(string Code, string? Description = null)
+    {
+        public static Error None => new(string.Empty);
+
+        public static implicit operator Result(Error error) => Result.Failure(error);
+    }
+}
